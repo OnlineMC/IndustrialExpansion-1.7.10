9606588a841ef19b86aa245302839a7abcd8d555
niko <1421312987@qq.com>
2018-01-21 16:05:03 +0000

    添加新装甲

--- src/main/java/cf/brforgers/mods/IndustrialExpansion/items/QuantumArmor.java
+++ src/main/java/cf/brforgers/mods/IndustrialExpansion/items/QuantumArmor.java
@@ -19,6 +19,7 @@ import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.ISpecialArmor;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
@@ -32,13 +33,14 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
     public static final ArmorProperties QUANTUM = new ArmorProperties(0, 0.2D, Integer.MAX_VALUE);
 
 
-
     /**
      * 装甲总能量
      */
     public int maxEnergy = 2000000;
 
-    /**最大充电速度*/
+    /**
+     * 最大充电速度
+     */
     public int maxTransfer = 1000;
 
     public double absorbRatio = 1.0E7D; //0.9D
@@ -46,20 +48,21 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
      * 每点伤害扣除的RF
      */
     public int energyPerDamage = 20000;
+    private int constArmorType = -1;
     private int speedLevel;
     private int damageBoostLevel;
     private int nightVisionLevel;
     private int jumpLevel;
-    private boolean enableLegsEffect;
-    private boolean enablePlateEffect;
-    private boolean enableHelmetEffect;
-    private boolean enableBootsEffect;
+    private boolean enableLegsEffect = false;
+    private boolean enablePlateEffect = false;
+    private boolean enableHelmetEffect = false;
+    private boolean enableBootsEffect = false;
 
 //    public String[] textures = new String[2];
 
     public QuantumArmor(ArmorMaterial material, int type) {
-
         super(material, type);
+        this.constArmorType = type;
     }
 
     public QuantumArmor setEnergyParams(int maxEnergy, int maxTransfer) {
@@ -70,35 +73,84 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
         return this;
     }
 
-    public QuantumArmor setEnergyParams(int maxEnergy, int maxTransfer, int energyPerDamage) {
+    public QuantumArmor setEnergyParams(int maxEnergy, int maxTransfer, int energyPerDamage, int jumpLevel) {
         this.maxEnergy = maxEnergy;
         this.maxTransfer = maxTransfer;
         this.energyPerDamage = energyPerDamage;
-
         return this;
     }
 
-    public QuantumArmor setEffectSwitch(boolean enableLegsEffect ,boolean enablePlateEffect,boolean enableHelmetEffect,boolean enableBootsEffect){
-        this.enableLegsEffect = enableLegsEffect;
-        this.enablePlateEffect = enablePlateEffect;
-        this.enableHelmetEffect = enableHelmetEffect;
-        this.enableBootsEffect = enableBootsEffect;
+    /**
+     * 获得装备编号
+     */
+    public int getStackType() {
+        return constArmorType;
+    }
 
+    /**
+     * 根绝AconstArmorType返回对应的BUFF等级
+     */
+    public int getStackEffectLevel(int constArmorType) {
+        switch (constArmorType) {
+            case 0:
+                return this.nightVisionLevel;
+            case 1:
+                return this.damageBoostLevel;
+            case 2:
+                return this.speedLevel;
+            case 3:
+                return this.jumpLevel;
+            default:
+                return -1;
+        }
+    }
+
+    /**
+     * 根据constArrmorType设置装备的效果等级
+     */
+    public QuantumArmor setEffectLevel(int level) {
+        switch (constArmorType) {
+            case 0:
+                this.nightVisionLevel = level;
+                break;
+            case 1:
+                this.damageBoostLevel = level;
+                break;
+            case 2:
+                this.speedLevel = level;
+                break;
+            case 3:
+                this.jumpLevel = level;
+                break;
+        }
+        return this;
+    }
+
+    /**
+     * 根据constArrmorType设置是否启用这件装备的效果
+     */
+    public QuantumArmor setEffectSwitch(boolean isEnable) {
+        switch (constArmorType) {
+            case 0:
+                this.enableLegsEffect = isEnable;
+                break;
+            case 1:
+                this.enablePlateEffect = isEnable;
+                break;
+            case 2:
+                this.enableHelmetEffect = isEnable;
+                break;
+            case 3:
+                this.enableBootsEffect = isEnable;
+                break;
+        }
         return this;
     }
 
 
-    public QuantumArmor setEffectLevel(int damageBoostLevel , int nightVisionLevel , int speedLevel,int jumpVisionLevel) {
-        this.damageBoostLevel = damageBoostLevel;
-        this.speedLevel = speedLevel;
-        this.nightVisionLevel = nightVisionLevel;
-        this.jumpLevel = jumpVisionLevel;
-
-        return this;
-    }
-
-
-    /**返回这个ItemStack是否能被铁砧修复*/
+    /**
+     * 返回这个ItemStack是否能被铁砧修复
+     */
     @Override
     public boolean getIsRepairable(ItemStack itemToRepair, ItemStack stack) {
 
@@ -109,8 +161,14 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
     @Override
     /**根据装备稀有等级返回颜色枚举*/
     public EnumRarity getRarity(ItemStack stack) {
+        String[] str = new String[]{"Common","Uncommon", "Rare","Epic"};
+        String[] data = stack.toString().split("\\|");
+        if(data[0].equals("QuantumArmor")){
+            int i= Integer.valueOf(data[2]);
+            if(i != -1 && i > str.length-1)return EnumRarity.valueOf(str[i]);
 
-        return EnumRarity.uncommon;
+        }
+        return EnumRarity.common;
     }
 
     @Override
@@ -169,7 +227,7 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
 
     @SuppressWarnings("unchecked")
     @Override
-    /**返回具有相同ID但不同元的项目列表（例如：染料会返回16项）*/
+    /**返回具有相同ID但不同元素的项目列表（例如：染料会返回16项）*/
     public void getSubItems(Item item, CreativeTabs tab, List list) {
 
         list.add(EnergyHelper.setDefaultEnergyTag(new ItemStack(item, 1, 0), 0));
@@ -188,7 +246,7 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
      */
     protected int getAbsorptionRatio() {
 
-        switch (armorType) {
+        switch (constArmorType) {
             case 0:
                 return 25;
             case 1:
@@ -201,37 +259,43 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
         return 0;
     }
 
-    /**返回每点伤害扣除的能量*/
+    /**
+     * 返回每点伤害扣除的能量
+     */
     protected int getEnergyPerDamage(ItemStack stack) {
 
         int unbreakingLevel = MathHelper.clamp(EnchantmentHelper.getEnchantmentLevel(Enchantment.unbreaking.effectId, stack), 0, 4);
         return energyPerDamage * (5 - unbreakingLevel) / 5;
     }
 
-    /**检索在计算装甲伤害时使用的修饰符。 更高优先级的装甲将在低优先级之前对其施加损害。 如果有多件同等优先级的装甲，则根据吸收率分配它们之间的伤害。
+    /**
+     * 检索在计算装甲伤害时使用的修饰符。 更高优先级的装甲将在低优先级之前对其施加损害。 如果有多件同等优先级的装甲，则根据吸收率分配它们之间的伤害。
+     *
      * @param player 具有生命值的实体
-     * @param armor 装甲的ItemStack实例
+     * @param armor  装甲的ItemStack实例
      * @param source 伤害来源
      * @param damage 对实体造成的总伤害
-     * @param slot 装甲所在的装备槽
+     * @param slot   装甲所在的装备槽
      * @return 返回装甲的受损信息 ArmorProperties 实例。
-     * */
-    @Override       /* ISpecialArmor */         
+     */
+    @Override       /* ISpecialArmor */
     public ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot) {
 
         if (source.damageType.equals("quantum")) {//伤害类型是否为quantum（量子）类型
             return QUANTUM;
         } else if (source.isUnblockable()) {
             int absorbMax = getEnergyPerDamage(armor) > 0 ? 25 * getEnergyStored(armor) / getEnergyPerDamage(armor) : 0;
-            return new ArmorProperties(0, absorbRatio * getArmorMaterial().getDamageReductionAmount(armorType) * 0.025, absorbMax);
+            return new ArmorProperties(0, absorbRatio * getArmorMaterial().getDamageReductionAmount(constArmorType) * 0.025, absorbMax);
         }
         int absorbMax = getEnergyPerDamage(armor) > 0 ? 25 * getEnergyStored(armor) / getEnergyPerDamage(armor) : 0;
-        return new ArmorProperties(0, absorbRatio * getArmorMaterial().getDamageReductionAmount(armorType) * 0.05, absorbMax);
+        return new ArmorProperties(0, absorbRatio * getArmorMaterial().getDamageReductionAmount(constArmorType) * 0.05, absorbMax);
         // 0.05 = 1 / 20 (max armor)
     }
 
 
-    /**获得显示的装甲*/
+    /**
+     * 获得显示的装甲
+     */
     @Override
     public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot) {
         if (getEnergyStored(armor) >= getEnergyPerDamage(armor)) {
@@ -244,22 +308,41 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
     @Override
     public void onArmorTick(World world, EntityPlayer player, ItemStack armor) {
         if (world.isRemote) return;
-        if ( getEnergyStored(armor) >= getEnergyPerDamage(armor)) {    //装甲剩余电量 >= 装甲造成伤害扣除的电量
-            if (enableLegsEffect && armor.getItem() == ItemManager.itemLegs1) {         //裤子
-                player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 260, speedLevel, true));   //获得速度效果
+        if (getEnergyStored(armor) >= getEnergyPerDamage(armor)) {    //装甲剩余电量 >= 装甲造成伤害扣除的电量
+
+            String data = armor.getItem().toString();
+
+            String[] datas = data == null ? new String[]{"QuantumArmor|", "-1|", "-1"} : data.split("\\|");
+            for(String str : datas){
+                System.out.println(str);
             }
-            if (enablePlateEffect && armor.getItem() == ItemManager.itemPlate1) {           //PotionEffect(效果ID，持续时间，效果等级，是否是范围的？)
-                player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 260, damageBoostLevel, true));  //伤害提升
+            int[] num = new int[2];
+            num[0] = Integer.valueOf(datas[1]);
+            num[1] = Integer.valueOf(datas[2]);
+            if (datas[0].equals("QuantumArmor")) {
+                switch (num[0]) {
+                    case 0:
+                        if (enableHelmetEffect)
+                            player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 260, num[1], true));
+                    case 1:
+                        if (enablePlateEffect)
+                            player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 260, num[1], true));
+                    case 2:
+                        if (enableLegsEffect)
+                            player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 260, num[1], true));
+                    case 3:
+                        if (enableBootsEffect)
+                            player.addPotionEffect(new PotionEffect(Potion.jump.id, 260, num[1], true));
+
+                }
             }
-            if (enableHelmetEffect && armor.getItem() == ItemManager.itemHelmet1) {
-                player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 260, nightVisionLevel, true));
-            }
-            if (enableBootsEffect && armor.getItem() == ItemManager.itemBoots1){
-                player.addPotionEffect(new PotionEffect(Potion.jump.id,260, jumpLevel,true));
-            }
+
         }
     }
-    /**对指定的ItemStack施加伤害。 该方法负责减少物品的耐用性和堆叠数量。 如果堆叠已经耗尽，它将被自动清理。*/
+
+    /**
+     * 对指定的ItemStack施加伤害。 该方法负责减少物品的耐久和堆叠数量。 如果堆叠已经耗尽，它将被自动清理。
+     */
     @Override
     public void damageArmor(EntityLivingBase entity, ItemStack armor, DamageSource source, int damage, int slot) {
 
@@ -305,7 +388,9 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
         return extract;
     }
 
-    /**获得装甲剩余的电量*/
+    /**
+     * 获得装甲剩余的电量
+     */
     @Override
     public int getEnergyStored(ItemStack container) {
 
@@ -319,5 +404,11 @@ public class QuantumArmor extends ItemArmorAdv implements ISpecialArmor, IEnergy
     public int getMaxEnergyStored(ItemStack container) {
 
         return maxEnergy;
+    }
+
+    @Override
+    public String toString() {
+        int stackEffectLevel = getStackEffectLevel(constArmorType);
+        return constArmorType != -1 && stackEffectLevel != -1 ? "QuantumArmor|" + constArmorType + "|" + getStackEffectLevel(constArmorType) : null;
     }
 }
